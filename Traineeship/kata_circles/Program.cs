d6cfda7436d84130ce730e1e50094fe543b9f316
vsesvit <sfrol91@gmail.com>
2023-08-13 11:55:52 +0200

    kata 1 solving

--- Traineeship/kata_circles/Program.cs
+++ Traineeship/kata_circles/Program.cs
@@ -6,21 +6,19 @@ int[] reverse = new int[num.Length];
 
 for (int i = 0; i < num.Length; i++)
 {
-    a += num[i];
+    if ((num[i] >= 0) && (num[i] < 10))
+    {
+        a += num[i];
+    }
 }
-Console.WriteLine(a);
-
 int.TryParse(a, out int sumConvert);
 sum = sumConvert + 1;
 string sumForReverse = sum.ToString();
-Console.WriteLine(sumForReverse);
-Console.WriteLine(sumForReverse.GetType());
 
 for (int j = 0; j < sumForReverse.Length; j++)
 {
     element = sumForReverse[j].ToString();
-
     reverse[j] = int.Parse(element);
-    Console.WriteLine(reverse[j]);
+    //Console.WriteLine(reverse[j]);
 }
 
