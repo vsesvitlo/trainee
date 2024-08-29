14e59f5b99e3ab9c420a69eac131b064ddc21260
Svitlana Frolova <sfrol91@gmail.com>
2024-08-29 18:39:43 +0200

    Adding hometask 1,4

---
+++ Traineeship/29.08.2024 - 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, enter decimal positive number ");
+string step1 = Console.ReadLine();
+int step2 = int.Parse(step1.Substring(step1.Length-2, 1));
+Console.WriteLine(step2);
+int step3 = int.Parse(step1);
+double step4 = Convert.ToDouble(step3);
+double step5 = (step4 - (step4 % 10)) / 10;
+Console.WriteLine(step5);
