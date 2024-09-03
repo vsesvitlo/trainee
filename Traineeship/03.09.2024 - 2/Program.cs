8d90c410f1809acf2e52e3d16b603cefb467983f
Svitlana Frolova <sfrol91@gmail.com>
2024-09-03 19:27:54 +0200

    Solving the task 56

---
+++ Traineeship/03.09.2024 - 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Дано натуральне число. Знайдіть цифру, що стоїть в розряді
+ * десятків в десятковому записі числа (друга цифра, якщо рахувати з кінця запису).*/
+Console.WriteLine("Please, write the whole positive number");
+string step1 = Console.ReadLine();
+if (step1.Length > 1)
+{
+    int step2 = int.Parse(step1.Substring(step1.Length - 2, 1));
+    Console.WriteLine(step2);
+}
+Console.WriteLine(0);
+
