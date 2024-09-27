a61ee629df03c558db2fc50ea2bdae92fa3939f4
Svitlana Frolova <sfrol91@gmail.com>
2024-09-27 18:32:00 +0200

    Solving the task 6

---
+++ Traineeship/27.09-2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Завдання 6: Розбиття рядка на слова
+Напиши програму, яка розбиває рядок на окремі слова, використовуючи метод Split.
+Вхід: ("C# is awesome!")->Вихід: ("C#", "is", "awesome!")
+
+
+Split і пробіл
+*/
+
+Console.WriteLine("Please, enter any phrase");
+string step1 = Console.ReadLine();
+
+string[] step2 = step1.Split(' ');
+//var a = "";
+for(int i = 0; i < step2.Length; i++)
+{
+    Console.WriteLine(step2[i]);
+}
