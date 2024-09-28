d9fb0f15f9045deee5ec359ddfd39677c1646787
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 12:08:51 +0200

    Solving the task 208

---
+++ Traineeship/28.09 - 208/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишіть програму для виведення усіх цілих чисел від 20 до n включно (n > 20),
+ * де n - ціле число, яке вводить користувач.
+
+Вхідні дані:
+
+25
+Вихідні дані:
+
+20
+21
+22
+23
+24
+25
+*/
+
+Console.WriteLine("Please, enter the n - number > 20");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+if (n >= 20)
+{
+     for (int i = 20; i <= n; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("Please, enter the n more than 20");
+}
