fda8a65c985a3df276de0ee6f0d034250097412e
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 11:30:24 +0200

    Solving the task 205

---
+++ Traineeship/28.09 - 205/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишіть програму-таймер зворотного відліку, 
+ * яка запитує у користувача кількість секунд n, з якої слід починати відлік.
+
+Вхідні дані:
+
+5*/
+Console.WriteLine("Please, enter the number - the quantity of time (seconds)");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+for(int i = n; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Start!");
