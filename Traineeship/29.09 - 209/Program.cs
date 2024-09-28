94d3243aa27d4064c364a2c62c958be646c12ff9
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 13:42:43 +0200

    Solving the task 210

---
+++ Traineeship/29.09 - 209/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Напишіть програму для друку цілих чисел від n до 0 із 
+ * виведенням біля кожного числа кількості символів #, що дорівнює значенню числа.
+
+Вхідні дані:
+
+6
+Вихідні дані:
+
+6 ######
+5 #####
+4 ####
+3 ###
+2 ##
+1 #*/
+
+
+//Not solved
+Console.WriteLine("Please, enter the n");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+var result = "";
+for (int i = n; i > 0; i--)
+{
+    result += "#";
+    for (int j = 0; j < n; j++) {
+        
+       
+        
+    }
+    
+    Console.WriteLine(i + result);
+    // string nStr = i.ToString() + " " + j;
+    //result = nStr;
+
+
+}
+//Console.WriteLine(result);
+
+/*for (int i = n; i > 0; i--)
+{ 
+    //for (int j = 0; j < n; j++) {
+        
+        result += "#";
+        
+    //}
+    
+    Console.WriteLine(i + result);
+    // string nStr = i.ToString() + " " + j;
+    //result = nStr;
+
+*/
---
+++ Traineeship/29.09 - 210/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Визначити суму всіх чисел від 1 до n (1 ≤ n ≤ 100).
+
+Вхідні дані:
+
+100
+16
+1
+Вихідні дані:
+
+5050
+136
+1*/
+
+Console.WriteLine("Please, enter the n");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+int result = 0;
+for(int i = 0; i <= n; i++)
+{
+    result += i;
+}
+Console.WriteLine(result);
