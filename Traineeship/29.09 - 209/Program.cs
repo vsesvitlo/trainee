5a7624f8834ca603a734aa6ebde272d723b9c104
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 15:14:36 +0200

    The solving task 209

--- Traineeship/29.09 - 209/Program.cs
+++ Traineeship/29.09 - 209/Program.cs
@@ -19,21 +19,18 @@ Console.WriteLine("Please, enter the n");
 string step1 = Console.ReadLine();
 int n = int.Parse(step1);
 var result = "";
-for (int i = n; i > 0; i--)
+var ofResult = "";
+for (int x = 0; x < n; x++)
 {
     result += "#";
-    for (int j = 0; j < n; j++) {
-        
-       
-        
-    }
-    
-    Console.WriteLine(i + result);
-    // string nStr = i.ToString() + " " + j;
-    //result = nStr;
-
-
 }
+for (int i = 0; i < result.Length; i++)
+{
+   ofResult = result.Substring(i, result.Length - i);
+   Console.WriteLine(n + ofResult);
+    n--;
+}
+
 //Console.WriteLine(result);
 
 /*for (int i = n; i > 0; i--)
