44d8c2b1f8b9bff9d61765069e3f4d2ca45d90a6
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 15:07:57 +0200

    The solving task - 212

---
+++ Traineeship/29.09 - 212/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишіть програму для побудови шаблону як у вихідних даних за введеним значенням n.
+
+Вхідні дані:
+
+5
+Вихідні дані:
+
+*****
+****
+***
+**
+*
+
+ */
+
+Console.WriteLine("Please, enter the n");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+var result = "";
+var ofResult = "";
+for (int x = 0; x < n; x++)
+{
+    result += "*";
+}
+for (int i = 0; i < result.Length; i++)
+{
+    ofResult = result.Substring(i, result.Length - i);
+    Console.WriteLine(ofResult);
+}
