f1d37cedc7d9f98a07066a364a4098671a2d860f
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 11:21:16 +0200

    Solving the task 10

---
+++ Traineeship/28.09 - 4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Завдання 10: Зміна регістру
+Напиши програму, яка перетворює всі літери в рядку на великі або на малі.
+Вхід: ("Hello, World!") -> Вихід: ("HELLO, WORLD!")
+Вхід: ("Hello, World!") ->Вихід: ("hello, world!")*/
+
+
+Console.WriteLine("Please, enter any sentence");
+string step1 = Console.ReadLine();
+Console.WriteLine("Please, choose uppercase - enter 1; lowcase - enter 2 ");
+string step2 = Console.ReadLine();
+int step3 = int.Parse(step2);
+switch (step3)
+{
+    case 1:
+        string step4 = step1.ToUpper();
+        Console.WriteLine(step4);
+        break;
+    case 2:
+        string step5 = step1.ToLower();
+        Console.WriteLine(step5);
+        break;
+    default:
+        Console.WriteLine("Please, enter 1 or 2");
+        break;
+}
