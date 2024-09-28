da21ec000428f0ff5f4cefdc0632adde456fc19a
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 14:31:30 +0200

    Solving the task 211

---
+++ Traineeship/29.09 - 211/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишіть програму для побудови шаблону як у вихідних даних за введеним значенням n.
+
+Вхідні дані:
+
+9
+Вихідні дані:
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+*/
+
+Console.WriteLine("Please, enter the n");
+string step1 = Console.ReadLine();
+int n = int.Parse(step1);
+var result = "";
+for (int i = 1; i <= n; i++)
+{ 
+    result += i;
+    Console.WriteLine(result);
+} 
