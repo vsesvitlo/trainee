a4901ed31e69bfa49857e566d510c465c196e353
Svitlana Frolova <sfrol91@gmail.com>
2024-09-28 11:06:16 +0200

    Solving the task 9

---
+++ Traineeship/28.09 - 3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Завдання 9: Перевірка наявності слова на початку рядка
+Напиши програму, яка перевіряє, чи починається рядок з певного слова.
+Вхід: ("Welcome to C# programming", слово: "Welcome")->Вихід: ("True")
+Вхід: ("Hello World", слово: "Welcome")->Вихід: ("False")*/
+Console.WriteLine("Please, enter any sentence");
+string step1 = Console.ReadLine();
+Console.WriteLine("Please, enter word for check");
+string step2 = Console.ReadLine();
+bool isCheck = true;
+for (int i = 0, j = 0; j < step2.Length;  i++, j++)
+{
+    if (step1[i] != step2[j]) {
+        isCheck = false;
+    }
+}
+Console.WriteLine(isCheck);
