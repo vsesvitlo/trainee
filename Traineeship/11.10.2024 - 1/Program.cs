6519ec9d609f74c6eb969eac38d8df1847ae2625
Svitlana Frolova <sfrol91@gmail.com>
2024-10-12 12:31:33 +0200

    Additional solving

--- Traineeship/11.10.2024 - 1/Program.cs
+++ Traineeship/11.10.2024 - 1/Program.cs
@@ -21,9 +21,32 @@
             return third;
         }
 
+        public Vector CutVector(Vector cutter)
+        {
+            Vector fourth = new Vector(this.x - cutter.x, this.y - cutter.y, this.z - cutter.z);
+            return fourth;
+        }
+
+        public Vector MultiVector(Vector mult)
+        {
+            Vector fifth = new Vector(this.x * mult.x, this.y * mult.y, this.z * mult.z);
+            return fifth;
+        }
+
+        public Vector SqVector()
+        {
+
+            /*double a = Convert.ToDouble(this.x);
+            double b = Convert.ToDouble(this.y);
+            double c = Convert.ToDouble(this.z);*/
+            Vector sixth = new Vector((this.x * this.x), this.y * this.y , this.z * this.z);
+            return sixth;
+        }
+
+
         public override string? ToString()
         {
-            return this.x.ToString() + this.y.ToString() + this.z.ToString();
+            return this.x.ToString() + " " + this.y.ToString() + " "+ this.z.ToString();
         }
     }
 
@@ -40,9 +63,23 @@
 
             Vector third = first.AddVector(second);
             Console.WriteLine(third);
-           /* Console.WriteLine(third.x);
-            Console.WriteLine(third.y);
-            Console.WriteLine(third.z);*/
+
+            Vector fourth = first.CutVector(second);
+            Console.WriteLine(fourth);
+
+            Vector fifth = first.MultiVector(second);
+            Console.WriteLine(fifth);
+
+            Vector sixth = first.SqVector();
+            Console.WriteLine(sixth);
+
+            Vector seventh = second.SqVector();
+            Console.WriteLine(seventh);
+
+
+            /* Console.WriteLine(third.x);
+             Console.WriteLine(third.y);
+             Console.WriteLine(third.z);*/
         }
     }
 }
