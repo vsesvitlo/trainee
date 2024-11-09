3b5d75c41483cb0c42ba6580250ebc9c5eb01ad3
Svitlana Frolova <sfrol91@gmail.com>
2024-11-09 22:24:24 +0100

    Solving 406

--- Traineeship/09.11.2024 - 406/Program.cs
+++ Traineeship/09.11.2024 - 406/Program.cs
@@ -8,13 +8,28 @@
 Вихідні дані:
 
 7 3 4 10 2 9 4*/
+using System.Linq;
+
 namespace _09._11._2024___406
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] input = { 4, 10, 2, 9, 4, 7, 3 };
+            List<int> listExample = new List<int>(input);
+            int count = listExample.Count;
+            
+            List<int> listExample2 = new List<int> (listExample.Slice(0, count - 2));
+            List<int> listExample3 = new List<int>(listExample.Slice(count - 2, 2));
+            foreach (int i in listExample2)
+            {
+                listExample3.Add(i);
+            }
+           foreach (int i in listExample3)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
