126be0143531b1df88e600d50e3d204b592f5c23
Svitlana Frolova <sfrol91@gmail.com>
2024-11-10 10:37:50 +0100

    Solving the task 409

--- Traineeship/09.11.2024 - 409/Program.cs
+++ Traineeship/09.11.2024 - 409/Program.cs
@@ -16,7 +16,11 @@ namespace _09._11._2024___409
         {
             string[] input = { "Ukrainian", "French", "Bulgarian", "Norwegian", "Latvian" };
             List<string> listExample = new List<string>(input);
-            int count = listExample.Count;
+            listExample.Reverse();
+            foreach (string i in listExample)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
