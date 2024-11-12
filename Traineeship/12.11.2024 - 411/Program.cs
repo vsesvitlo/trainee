c159accf886ac6713577ca1d68ee83658beaa1fa
Svitlana Frolova <sfrol91@gmail.com>
2024-11-12 13:45:28 +0100

    Solving the task 412

---
+++ Traineeship/12.11.2024 - 411/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Виведіть всі елементи списку з парними індексами. 
+ * Вводиться список чисел. Всі числа списку знаходяться на одному рядку.
+
+Вхідні дані:
+
+1 2 3 4 5
+Вихідні дані:
+
+1 3 5*/
+
+namespace _12._11._2024___411
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 2, 3, 4, 5 };
+            List<int> listExample = new List<int>(input);
+            foreach (int i in listExample)
+            {
+                listExample.Add(i);
+            }
+            string result = string.Join(" ", listExample);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Traineeship/12.11.2024 - 412/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишіть програму, яка роздруковує лише ті елементи введеного списку,
+ * що менше введеного значення n. Список містить унікальні значення.
+
+Вхідні дані:
+
+2 7 11 3 8 90 144 15 5
+94
+Вихідні дані:
+
+2 7 11 3 8 90 15 5*/
+
+namespace _12._11._2024___412
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 2, 7, 11, 3, 8, 90, 144, 15, 5};
+            int k = 94;
+            List<int> listExample = new List<int>(input);
+            List<int> listExampleNew = new List<int>();
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i]< k)
+                {
+                    listExampleNew.Add(input[i]);
+                }
+            }
+            string result = string.Join(" ", listExampleNew);
+            Console.WriteLine(result);
+        }
+    }
+}
