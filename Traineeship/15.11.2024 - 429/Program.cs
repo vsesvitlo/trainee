2baa8b555516ac7d4eb48bd4a796a5ba85f09f46
Svitlana Frolova <sfrol91@gmail.com>
2024-11-15 15:12:10 +0100

    Solving the task 429

---
+++ Traineeship/15.11.2024 - 429/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишіть програму для підрахунку кількості днів, 
+ * в яких температура була не нижче, ніж середня температура за весь період. 
+ * У першому рядку вводиться список показників температури на кожен день.
+ * У рядку виведення одне число - кількість днів, які відповідають умові.
+
+Вхідні дані:
+
+-3 -1 0 2 6 8 12 15
+Вихідні дані:
+
+4*/
+namespace _15._11._2024___429
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { -3, -1, 0, 2, 6, 8, 12, 15 };
+            List<int> listExample = new List<int>(input);
+            int days = 0;
+            int total = 0;
+            for (int i = 0; i < listExample.Count; i++)
+            {
+                total += listExample[i];
+                
+            }
+            int average = total/ listExample.Count;
+            for (int j = 0; j < listExample.Count; j++)
+            {
+                if(listExample[j] > average)
+                {
+                    days++;
+                }
+            }
+            Console.WriteLine(days);
+        }
+    }
+}
