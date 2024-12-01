0601f6d1bc52a57050d8015b62d4c229197e38d2
Svitlana Frolova <sfrol91@gmail.com>
2024-12-01 14:49:18 +0100

    first step

---
+++ Traineeship/01.12.2024 - explicit, implicit/Program.cs
@@ -0,0 +1,41 @@
+﻿/*завдання на оператори перетворення типів(explicit, implicit):
+Створіть програму для моделювання перетворення між двома класами: 
+Temperature (температура в градусах Цельсія) і Fahrenheit (температура в градусах Фаренгейта).
+
+ Реалізуйте клас Temperature, який зберігає значення температури у градусах Цельсія.
+Реалізуйте клас Fahrenheit, який зберігає значення температури у Фаренгейтах.
+Реалізуйте:
+Оператор explicit для явного перетворення між Temperature та Fahrenheit.
+Оператор implicit для неявного перетворення між Fahrenheit та Temperature.
+Додайте методи для виводу значень температури в обох форматах.*/
+
+namespace _01._12._2024___explicit__implicit
+{
+    class Temperature
+    {
+        public double Celsuis;
+
+        public Temperature(double celsuis)
+        {
+            Celsuis = celsuis;
+        }
+    }
+    public class Fahrenheit
+    {
+        public double Fahr;
+
+        public Fahrenheit(double fahrenheit)
+        {
+            Fahr = fahrenheit;
+        }
+    }
+    public static explicit operator double Temperature () { return ; }
+    public static implicit operator double (исходный_тип v) { return значение; }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
