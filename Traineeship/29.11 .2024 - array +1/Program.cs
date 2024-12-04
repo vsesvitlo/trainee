62f15217bdaf3c95327bc83839397756fdf5926d
Svitlana Frolova <sfrol91@gmail.com>
2024-12-04 14:30:00 +0100

    Solving the task

--- Traineeship/29.11 .2024 - array +1/Program.cs
+++ Traineeship/29.11 .2024 - array +1/Program.cs
@@ -6,15 +6,16 @@ namespace _29._11_._2024___array__1
     {
         static void Main(string[] args)
         {
-            int[] num1 =  { 8, 9, 9, 9};
-            int[] newNum1 = { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 5, 3, 2, 6, 7, 8, 4, 2, 4, 2, 6, 7, 8, 7, 4, 5, 2, 2 };
+           int[] num1 =  { 7, 9};
+           // int[] num1 = { 1, 2, 9};
+           // int[] num1 = { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7, 5, 3, 2, 6, 7, 8, 4, 2, 4, 2, 6, 7, 8, 7, 4, 5, 2, 2 };
             int [] num2 = Kata.UpArray(num1);
-            int[] newNum2 = Kata.UpArray(newNum1);
+            //int[] newNum2 = Kata.UpArray(newNum1);
             for (int k = 0; k < num2.Length; k++)
             {
                 Console.WriteLine((num2[k]));
             }
-            for (int k = 0; k < newNum2.Length; k++)
+            //for (int k = 0; k < newNum2.Length; k++)
             {
                 //Console.WriteLine((newNum2[k]));
             }
@@ -62,30 +63,37 @@ namespace _29._11_._2024___array__1
                 newNum[0] = 1;
                 return newNum;
             }
-            for (int i = num.Length - 1; i > 0; i--) //? 72 - 
+            for (int i = 1; i < num.Length; i++)
             {
+                newNum[0] = num[0];
 
-                if ( num[i] < 0 || num[i] > 9)
+                if (num[i] < 0 || num[i] > 9)
                 {
                     return null;
                 }
                 if (num[i] == 9)
+                {
+                    newNum[i] = 0;
+                    z = num[i - 1] + 1;
+                    newNum[i - 1] = z;
+                    if (newNum[0] == z)
                     {
-                        newNum[i] = 0;
-                        z = num[i - 1] + 1;
-                        newNum[i - 1] = z;
-                    if (newNum[0] == z )
                         break;
                     }
+                    newNum[0] = num[0];
+                    break;
+                }
 
-                else {
+                else
+                {
+                    newNum[0] = num[0];
                     newNum[i] = num[i];
                     newNum[newNum.Length - 1] = num[num.Length - 1] + 1;
                 }
-                
-                
+
             }
             return newNum;
+            
         }
     }
 }
