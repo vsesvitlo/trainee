9d58d7e9c1d6c470ec1c21ebac1b15805ed14055
Svitlana Frolova <sfrol91@gmail.com>
2024-12-12 13:43:28 +0100

    Solving the task _ list, loop

---
+++ Traineeship/12.12.2024 - list/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача: 
+є набір продуктів які хочеться купити 
+{"Сік", "Вода", "Хліб", "Мясо", "Гриби"}
+
+і є магазини із продуктами: 
+{"Вода", "Хліб", "Молоко", "Морозиво"}
+{"Цукор", "Сіль", "Паприка", "Перець"}
+{"Вафлі", "Бублик", "Інжир", "Хліб"}
+
+сказати "Yes" - якщо хоча б 1 продукт в магазині є із бажаного списку, 
+інакше сказати "No"*/
+namespace _12._12._2024___list
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = { "Сік", "Вода", "Хліб", "Мясо", "Гриби" };
+            List<string> products = new List<string>(input);
+            string[] input1 = { "Вода", "Хліб", "Молоко", "Морозиво" };
+            string[] input2 = { "Цукор", "Сіль", "Паприка", "Перець" };
+            string[] input3 = { "Вафлі", "Бублик", "Інжир", "Хліб" };
+            List<string> productsShop = new List<string>(input1);
+            string positive = "Yes";
+            string negative = "No";
+            string result = "";
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (productsShop.Contains(products[i]) )
+                {
+                    result = positive;
+                    break;
+                }
+                else
+                {
+                    result = negative;
+                   
+                }
+            }
+            Console.Write(result);
+        }
+    }
+}
