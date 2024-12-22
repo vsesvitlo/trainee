b941cfb8e6ddb96bfada38b9313e3ec2dfa18db1
Svitlana Frolova <sfrol91@gmail.com>
2024-12-22 16:29:09 +0100

    Solving Heron's formula task

---
+++ Traineeship/22.12.2024 - heron formula/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+Heron's formula:
+
+s
+∗
+(
+s
+−
+a
+)
+∗
+(
+s
+−
+b
+)
+∗
+(
+s
+−
+c
+)
+s∗(s−a)∗(s−b)∗(s−c)
+​
+ 
+where
+
+s
+=
+a
++
+b
++
+c
+2
+s= 
+2
+a+b+c
+​
+ 
+Output should have 2 digits precision.*/
+namespace _22._12._2024___heron_formula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 4;   
+            double c = 5;
+            double result = Kata.Heron(a,b, c); //6
+            Console.WriteLine(result);
+        }
+    }
+    public class Kata
+    {
+        public static double Heron(double a, double b, double c)
+        {
+
+            double s = Math.Round(((a + b + c) / 2), 2);
+            double result = Math.Round(Math.Sqrt(s * (s - a) * (s - b) * (s - c)), 2);
+            return result;
+        }
+    }
+}
